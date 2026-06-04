d35076cea9137042f80d282affd676b010cbc7ee
saros-dev <shojaiisaros@yahoo.ca>
2026-06-04 19:57:01 +0330

    Add monitoring with Prometheus and Grafana

---
+++ todo-app/terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:alpine"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "terraform-nginx"
+
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
